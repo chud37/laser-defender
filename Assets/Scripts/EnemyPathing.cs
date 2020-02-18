a93bd2a9eedb2dcefa1bbb189640b9e6e3cc0a84
chud37 <chud37@hotmail.com>
2020-02-18 07:15:46 +0000

    Add files via upload

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    int wayPointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[wayPointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (wayPointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[wayPointIndex].transform.position;
+            var movementThisFrame = Time.deltaTime * moveSpeed;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+            if (transform.position == targetPosition)
+            {
+                wayPointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
